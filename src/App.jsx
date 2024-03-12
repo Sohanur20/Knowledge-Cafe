9d806d119c60be4a52f9309f06cee1cdab8c82dc
Sohanur20 <mdsohank168@gmail.com>
2024-03-12 22:02:19 +0600

    projects setup done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-center bg-red-200 text-4xl">Knowledge Cafe</h1>
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
